7565005f7c1f938a2595458c45124c081e98972d
Tejas1kawale <55426899+Tejas1kawale@users.noreply.github.com>
2022-12-24 10:51:33 +0530

    comming videostream

--- app/src/main/java/com/example/multivideos/MainActivity.java
+++ app/src/main/java/com/example/multivideos/MainActivity.java
@@ -10,7 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("tejas");
-        System.out.println("Shubham");
+        System.out.println("yash");
+
+
     }
 }
