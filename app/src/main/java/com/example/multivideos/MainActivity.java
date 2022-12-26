ee1ecdf089ebd8f403b5035bd98c83fa454c8072
Tejas1kawale <55426899+Tejas1kawale@users.noreply.github.com>
2022-12-26 12:20:39 +0530

    comming videostream

--- app/src/main/java/com/example/multivideos/MainActivity.java
+++ app/src/main/java/com/example/multivideos/MainActivity.java
@@ -1,17 +1,52 @@
 package com.example.multivideos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.exoplayer2.util.Log;
 
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG="TAG";
+    RecyclerView videoList;
+    VideoAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("yash");
+        videoList =findViewById(R.id.videoList);
+        videoList.setLayoutManager(new LinearLayoutManager(this));
+        adapter=new VideoAdapter();
+        videoList.setAdapter(adapter);
+        getJsonData();
 
+    }
 
+    private void getJsonData() {
+        String URL="http://192.168.200.195:4000/videos";
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        JsonObjectRequest objectRequest=new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+               Log.d(TAG,"OnerrorResponse"+response);
+            }
+        },new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                Log.e(TAG,"OnErrorResponse"+error.getMessage());
+            }
+        });
+        requestQueue.add(objectRequest);
     }
 }
