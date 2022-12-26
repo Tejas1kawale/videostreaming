4661b1f8413b4c2d49ddfe6d7e6e589e2ee150a4
Tejas1kawale <55426899+Tejas1kawale@users.noreply.github.com>
2022-12-26 13:50:21 +0530

    comming videostream

--- app/src/main/java/com/example/multivideos/MainActivity.java
+++ app/src/main/java/com/example/multivideos/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -15,6 +16,8 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.exoplayer2.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,7 +42,28 @@ public class MainActivity extends AppCompatActivity {
         JsonObjectRequest objectRequest=new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-               Log.d(TAG,"OnerrorResponse"+response);
+                try {
+                    JSONArray categories=response.getJSONArray("videos");
+
+                    Log.d(TAG,"changeResponse"+categories);
+                    for(int i=0;i<categories.length();i++)
+                    {
+                        JSONObject video=categories.getJSONObject(i);
+
+                        video v=new video();
+                        v.setId(video.getString("id"));
+                        v.setName(video.getString("name"));
+                        String url="http://192.168.200.195:4000/video/";
+                        url=url.concat(video.getString("id"));
+                        Log.d(TAG, "url=: "+url);
+                        v.setVideoUrl(url);
+
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
             }
         },new Response.ErrorListener(){
             @Override
---
+++ app/src/main/java/com/example/multivideos/video.java
@@ -0,0 +1,31 @@
+package com.example.multivideos;
+
+public class video {
+    private  String id;
+    private  String name;
+    private  String videoUrl;
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
